e98a9ee62013b4efe859af1dea1cd5f3a78b9179
Mehmettarhan <87024517+Mehmettarhan@users.noreply.github.com>
2021-07-26 10:32:31 -0500

    work 4 is done

--- src/day1/Second.java
+++ src/day1/Second.java
@@ -6,6 +6,7 @@ public class Second {
         // work 1 is done here
         // Zahra is gone today
         // Katie is with my wife today.
+        // Zahra is going to have fun with Katie in Milwaukee
 
 
     }
