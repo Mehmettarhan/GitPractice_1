08c4662119f7fcee197f7239ea08f2deb6d76e31
Mehmettarhan <87024517+Mehmettarhan@users.noreply.github.com>
2021-07-29 13:46:25 -0500

    last commit made

--- src/day3/SetPractice.java
+++ src/day3/SetPractice.java
@@ -4,6 +4,7 @@ public class SetPractice {
     public static void main(String[] args) {
         // set class is coll
         // set is a collection type that only store unique item
+        // We should use set method more
 
     }
 }
