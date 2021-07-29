3f680bf23c9dc868df163b39e82073208b3f475a
Mehmettarhan <87024517+Mehmettarhan@users.noreply.github.com>
2021-07-29 13:45:27 -0500

    Set store unique item

--- src/day3/SetPractice.java
+++ src/day3/SetPractice.java
@@ -3,6 +3,7 @@ package day3;
 public class SetPractice {
     public static void main(String[] args) {
         // set class is coll
+        // set is a collection type that only store unique item
 
     }
 }
